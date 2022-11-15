def13593adf3ce2af312861b6315d5e97bb167e5
Alejandro Cespedes <alejandroc3001@gmail.com>
2022-11-14 21:22:58 -0500

    fusion del menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
+const burguerMenu = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener('mouseover', toggleDesktopMenu);
+burguerMenu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('inactive')
 }
