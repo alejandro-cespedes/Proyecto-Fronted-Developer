3d763d7750c18d38ce6e55dbbbc23e859fd0e80d
Alejandro Cespedes <alejandroc3001@gmail.com>
2022-11-15 19:04:09 -0500

    agregando el carrito de compras

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktopMenu = document.querySelector('.desktop-menu')
 const burguerMenu = document.querySelector('.menu')
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart')
 const mobileMenu = document.querySelector('.mobile-menu')
+const aside = document.querySelector('.my-order')
+
+
 
 menuEmail.addEventListener('mouseover', toggleDesktopMenu);
 burguerMenu.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCartAside);
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive')
+function toggleDesktopMenu() 
+{
+    aside.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
 }
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive')
+
+function toggleMobileMenu()
+{
+    aside.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive');
 }
+
+function toggleCartAside()
+{
+    desktopMenu.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    aside.classList.toggle('inactive');
+}
+
+
